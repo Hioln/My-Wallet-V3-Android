0f710f44c5f276eae4891bd2caa75dd74da68a09
Adam Bennett <ditn@users.noreply.github.com>
2018-04-06 13:52:44 +0100

    Revert "fix(Dashboard): Only refresh ticker and balances on broadcast receive…"

--- app/src/main/java/piuk/blockchain/android/ui/dashboard/DashboardPresenter.kt
+++ app/src/main/java/piuk/blockchain/android/ui/dashboard/DashboardPresenter.kt
@@ -109,16 +109,6 @@ class DashboardPresenter @Inject constructor(
                 )
     }
 
-    fun updateBalances() {
-
-        with(view) {
-            scrollToTop()
-        }
-
-        updatePrices()
-        updateAllBalances()
-    }
-
     override fun onViewDestroyed() {
         rxBus.unregister(MetadataEvent::class.java, metadataObservable)
         super.onViewDestroyed()
